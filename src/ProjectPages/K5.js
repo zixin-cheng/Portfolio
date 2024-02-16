d91d771edeb59306e7c89135c7f3f7395191a2fb
zixin-cheng <janecheng1026@gmail.com>
2024-02-15 20:09:28 -0500

    Font

--- src/ProjectPages/K5.js
+++ src/ProjectPages/K5.js
@@ -21,7 +21,7 @@ export default function K5() {
 
         <Paragraph
           content={`
-          This visual journalism project illuminates the deployment of a new robot by the NYPD in the Times Square subway station. Utilizing a scroll interaction combined with 3D graphics on a webpage, the project delves into the implications surrounding this robotic presence. As a creative developer, my responsibility is to ensure that the visuals harmonize with the article content and offer a seamless interactive experience as users scroll through the webpage.
+          This visual journalism project illuminates the deployment of the new K-5 robot by the NYPD in the New York Times Square subway station. Utilizing a scroll interaction combined with 3D graphics on a webpage, the project delves into the implications surrounding this robotic presence. As the lead creative developer, my responsibility is to ensure that the visuals harmonize with the article content and offer a seamless interactive experience as users scroll through the webpage.
           <br><br>
           Explore the documentation page <a href="https://www.notion.so/janecheng/K-5-b5a9634fea604c2ea77eea094ec05a0d?pvs=4" target="_blank" rel="noopener noreferrer">here</a>.`}
         />
